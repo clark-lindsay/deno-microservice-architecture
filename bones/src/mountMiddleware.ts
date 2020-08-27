a4530f335af1c4aa33c77609af6f10a1e707188b
clark-lindsay <clarklindsay96@gmail.com>
2020-08-27 16:54:54 -0400

    changed function name to better reflect the destination of the data

--- bones/src/mountMiddleware.ts
+++ bones/src/mountMiddleware.ts
@@ -2,7 +2,7 @@ import { uuid } from "./deps.ts";
 import { Application, Context } from "https://deno.land/x/oak/mod.ts";
 
 export function mountMiddleware(app: Application) {
-  app.use(applyLocalContextToState);
+  app.use(applyLocalContextToDisplayState);
   app.use(setTraceIdHeader);
 }
 
@@ -10,9 +10,9 @@ function setTraceIdHeader(ctx: Context) {
   ctx.request.headers.set("TraceId", uuid.v4.generate());
 }
 
-async function applyLocalContextToState(
+async function applyLocalContextToDisplayState(
   ctx: Context,
-  next: () => Promise<void>,
+  next: () => Promise<void>
 ) {
   await next();
   ctx.state["traceId"] = ctx.request.headers.get("TraceId") ?? null;
